7c4d7d1ef56c5e6bb643f5e74ba5de50135edf01
Chris G <griggsca91@gmail.com>
2017-09-21 22:03:43 -0400

    giant update, code cleanup

--- public/javascript/app.jsx
+++ public/javascript/app.jsx
@@ -1,17 +1,92 @@
 import { default as React, Component } from 'react';
 import { render } from 'react-dom';
+import { default as request } from 'superagent';
+
+class Operator extends Component {
+
+  constructor(props) {
+    super(props);
+    this.toggleAvailability = this.toggleAvailability.bind(this);
+  }
+
+  toggleAvailability() {
+    this.props.toggleAvailability(this.props.data.loginID, 1, this.props.data.clientID, this.props.data.chatService);
+  }
+
+  render() {
+
+    let className = this.props.data.chatService ? "btn-success" : "btn-warning";
+    let buttonContent = this.props.data.chatService ? "Chat Available" : "Chat Unavailable";
+    return (
+      <div className="row justify-content-start">
+        <div className="col-2">
+          {this.props.data.name}
+        </div>
+        <div className="col-2">
+        <button onClick={this.toggleAvailability} className={className} id={this.props.data.loginID}>{buttonContent}</button>
+        </div>
+      </div>
+    )
+  }
+}
 
 class OperatorsBox extends Component {
 
+  constructor(props){
+    // Pass props to parent class
+    super(props);
+    // Set initial state
+    this.state = {
+      operators: [],
+      isLoading: false
+    }
+    this.getOperators = this.getOperators.bind(this);
+    this.toggleAvailability = this.toggleAvailability.bind(this);
+
+  }
+  componentDidMount(){
+    this.getOperators();
+  }
+
+
+  toggleAvailability(operatorID, serviceTypeID, clientID, statusType) {
+    request
+      .post('/api/setOperatorAvailability')
+      .send({operatorID: operatorID, serviceTypeID: serviceTypeID, clientID:clientID, statusType:statusType ? 1 : 2})
+      .set('accept', 'json')
+      .end((err, res) => {
+        this.getOperators();
+      });
+  }
+
+
+  getOperators() {
+    this.setState({isLoading: true});
+    request
+      .get("/api/getOperators")
+      .end((err, res) => {
+        this.setState({operators: res.body.data, isLoading: false})
+      });
+  }
+
   render() {
+
+
+    let operators = this.state.operators.map(i => {
+      return <Operator data={i} key={i.loginID} toggleAvailability={this.toggleAvailability}/>
+    });
+
     return (
-        <div className="commentBox">
+        <div className="container">
+          <h3>Current Operators</h3>
+          {operators.length == 0 && this.state.isLoading == false &&
+            <p>There aren't any operators logged in right now.</p>
+          }
+          {operators}
         </div>
     );
   }
 }
-
-
 
 render(
     <OperatorsBox />,
--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,53 @@ import Router from 'koa-router';
 import CryptoJS from 'crypto-js';
 import http from 'https';
 
+
+/**
+ *  Account ID: 706505873793485629
+ * API Settings: 1645412287691666032
+ * API Key: 4UQadjX14fasZR0f/VTKLGfM1VMV9oaOIcK5bLppG93o8ICp4Fx4p2G6lvY2ztqrwP9R7lenJ5MWyJIGCvHicg==
+ * 
+ * 
+ * 
+ */
+
 const router = new Router(); // require('koa-router')()
+
+const ServiceTypeChat = 1,
+  ServiceTypeEmail = 3,
+  ServiceTypeTicket = 5,
+  ServiceTypeSMS = 9,
+  ServiceTypeTwitter = 10;
+
+const StatusTypeLoggedOut = 0,
+  StatusTypeAway = 1,
+  StatusTypeAvailable = 2;
+
 const accountId = "706505873793485629";
 const apiSettings = "1645412287691666032";
 const apiKey = "4UQadjX14fasZR0f/VTKLGfM1VMV9oaOIcK5bLppG93o8ICp4Fx4p2G6lvY2ztqrwP9R7lenJ5MWyJIGCvHicg==";
-const auth = accountId + ':' + apiSettings + ':' + (new Date()).getTime();
-const authHash = auth + ':' + CryptoJS.SHA512(auth + apiKey).toString(CryptoJS.enc.Hex);
 
-
+/**
+ * getRequestURL is a utility function that formats the api URL to be used based on the request type and parameters
+ * 
+ * @param {any} requestType 
+ * @param {any} parameters 
+ * @returns 
+ */
 let getRequestURL = (requestType, parameters) => {
+  const auth = accountId + ':' + apiSettings + ':' + (new Date()).getTime();
+  const authHash = auth + ':' + CryptoJS.SHA512(auth + apiKey).toString(CryptoJS.enc.Hex);
+
   let url = "https://api.boldchat.com/aid/" + accountId + "/data/rest/json/v1/" + requestType + "?auth=" + authHash + (parameters.length > 0 ? ("&" + parameters) : "");
   return url
 }
 
+/**
+ * getNumberOfMobileViewers gets the number of visitors that are on mobile devices by sniffing the user agent.  
+ * It also uses a hardcoded folder ID value, if there is a more dynamic way it should be implemented.
+ * 
+ * @returns A promise that resolves to a number
+ */
 let getNumberOfMobileViewers = () => {
   const requestType = "getActiveVisits";
   const parameters = "FolderID=699623076574954132";
@@ -54,7 +88,7 @@ let getNumberOfDesktopViewers = () => {
           return prev + (cur.UserAgent.includes("Mobile") ? 0 : 1);
         }, 0);
         resolve(number);
-     });
+      });
 
     });
   });
@@ -83,33 +117,16 @@ let getActiveChats = () => {
 
 }
 
-router.get('/', async (ctx) => {
 
-  /**
-   *  Account ID: 706505873793485629
-   * API Settings: 1645412287691666032
-   * API Key: 4UQadjX14fasZR0f/VTKLGfM1VMV9oaOIcK5bLppG93o8ICp4Fx4p2G6lvY2ztqrwP9R7lenJ5MWyJIGCvHicg==
-   * 
-   * 
-   * 
-   */
-
-
-  let activeChats = await getActiveChats();
-  let mobileVisitors = await getNumberOfMobileViewers();
-  let desktopVisitors = await getNumberOfDesktopViewers();
-  await ctx.render('index', {
-    title: 'Hello Koa 2!',
-    activeChats: activeChats,
-    mobileVisitors: mobileVisitors,
-    desktopVisitors: desktopVisitors,
-  });
-});
-
+/**
+ * Gets a list of active operators
+ * 
+ * @returns a list of operators 
+ */
 let getOperators = () => {
 
-  const requestType = "getOperators";
-  const parameters = "";
+  const requestType = "getOperatorAvailability";
+  const parameters = `ServiceTypeID=${ServiceTypeChat}`;
 
   const location = getRequestURL(requestType, parameters);
 
@@ -126,13 +143,9 @@ let getOperators = () => {
         for (let d of par.Data) {
           let r = {
             loginID: d.LoginID,
+            clientID: d.ClientID,
             name: d.Name,
-            twitterService: d.TwitterService,
-            emailService: d.EmailService,
-            chatService: d.ChatService,
-            smsService: d.SmsService,
-            facebookService: d.FacebookService,
-            ticketService: d.TicketService,
+            chatService: d.StatusType == StatusTypeAvailable,
           }
           result.push(r);
         }
@@ -145,7 +158,72 @@ let getOperators = () => {
 };
 
 
-router.get('/api/getOperators', async (ctx) => {
+
+/**
+ * setOperatorStatus sets the availability of the operator for a specific service.
+ * Helper function to interact with the BoldChat api.
+ * 
+ * @param {any} operatorID 
+ * @param {any} serviceTypeID 
+ * @param {any} clientID 
+ * @param {any} statusType 
+ * @returns a Promise with the result of the update
+ */
+let setOperatorStatus = (operatorID, serviceTypeID, clientID, statusType) => {
+
+  let params = [
+    `ServiceTypeID=${serviceTypeID}`,
+    `OperatorID=${operatorID}`,
+    `ClientID=${clientID}`,
+    `StatusType=${statusType}`,
+  ];
+  let url = getRequestURL("setOperatorAvailability", params.join("&"))
+  console.log(url);
+  return new Promise(resolve => {
+    http.get(url, (res) => {
+      let data = "";
+      res.on("data", function (chunk) {
+        data += chunk;
+      });
+      res.on('end', function () {
+        let par = JSON.parse(data);
+        console.log(par);
+        resolve(par.Status);
+      });
+
+    });
+  });
+
+
+}
+
+/**
+ * ROUTES
+ */
+router.get('/', async(ctx) => {
+  let activeChats = await getActiveChats();
+  let mobileVisitors = await getNumberOfMobileViewers();
+  let desktopVisitors = await getNumberOfDesktopViewers();
+  await ctx.render('index', {
+    title: 'Hello Koa 2!',
+    activeChats: activeChats,
+    mobileVisitors: mobileVisitors,
+    desktopVisitors: desktopVisitors,
+  });
+});
+
+
+
+router.post('/api/setOperatorAvailability', async(ctx) => {
+  let d = ctx.request.body;
+  let operators = await setOperatorStatus(d.operatorID, d.serviceTypeID, d.clientID, d.statusType);
+
+  ctx.body = {
+    data: operators,
+  };
+});
+
+router.get('/api/getOperators', async(ctx) => {
   let operators = await getOperators();
 
   ctx.body = {
